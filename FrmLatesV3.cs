aeeb4b5699f7f359ad60e93b53352dccfeef0c84
Hattie Collinge <220590@live.brock.ac.uk>
2023-10-03 11:36:12 +0100

    jqwjld

--- FrmLatesV3.cs
+++ FrmLatesV3.cs
@@ -20,31 +20,7 @@ namespace StudentLatesApp
 
         private void FrmLatesV3_Load(object sender, EventArgs e)
         {
-            int period = 0, minsLate = 0, minsFrom9am = 0;
-            DateTime nineOclockDate = DateTime.Today; // the time at midnight this morning
-            nineOclockDate = nineOclockDate.AddHours(9); // the date and time at 9:00am
-            minsFrom9am = Convert.ToInt32((DateTime.Now - nineOclockDate).TotalMinutes);
-            if (minsFrom9am > 0 && minsFrom9am < 89)
-            {
-                period = 1;
-                minsLate = minsFrom9am;
-            }
-            else if (minsFrom9am > 110 && minsFrom9am < 199)
-            {
-                period = 2;
-                minsLate = minsFrom9am - 110;
-            }
-            else if (minsFrom9am > 250 && minsFrom9am < 339)
-            {
-                period = 3;
-                minsLate = minsFrom9am - 250;
-            }
-            else if (minsFrom9am > 350 && minsFrom9am <439)
-            {
-                period = 4;
-                minsLate = minsLate - 350;
-            }
-
+           
 
 
                 // list to hold the studetid and the student name
@@ -73,9 +49,34 @@ namespace StudentLatesApp
 
         private void btnSaveLate_Click(object sender, EventArgs e)
         {
+            int period = 0, minsLate = 0, minsFrom9am = 0;
+            DateTime nineOclockDate = DateTime.Today; // the time at midnight this morning
+            nineOclockDate = nineOclockDate.AddHours(9); // the date and time at 9:00am
+            minsFrom9am = Convert.ToInt32((DateTime.Now - nineOclockDate).TotalMinutes);
+            if (minsFrom9am > 0 && minsFrom9am < 89)
+            {
+                period = 1;
+                minsLate = minsFrom9am;
+            }
+            else if (minsFrom9am > 110 && minsFrom9am < 199)
+            {
+                period = 2;
+                minsLate = minsFrom9am - 110;
+            }
+            else if (minsFrom9am > 250 && minsFrom9am < 339)
+            {
+                period = 3;
+                minsLate = minsFrom9am - 250;
+            }
+            else if (minsFrom9am > 350 && minsFrom9am < 439)
+            {
+                period = 4;
+                minsLate = minsLate - 350;
+            }
+
             clsDBConnector dbConnector = new clsDBConnector();
             string cmdStr = $"INSERT INTO tblLate  (studentID,period, dateOfLate,minsLate) " +
-                $"VALUES ('{cmbStudentID.SelectedValue}' , '{}', '{dtpLateDate.Value.Date}','{txtMinsLate.Text}')";
+                $"VALUES ('{cmbStudentID.SelectedValue}' , '{period}', '{}','{minsLate}')";
             dbConnector.Connect();
             dbConnector.DoDML(cmdStr);
             dbConnector.Close();
